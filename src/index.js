9e85d9a2f5a8e53abb1d9e0eed1ccda264d05e24
Filip Hlavac <filip.hlavac01@gmail.com>
2021-11-30 13:07:41 +0100

    Import Patternfly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '@patternfly/react-core/dist/styles/base.css';
 
 ReactDOM.render(
   <React.StrictMode>
