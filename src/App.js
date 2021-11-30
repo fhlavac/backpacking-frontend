3b33660d547b47a7078cff670091a486d4fba2db
Filip Hlavac <filip.hlavac01@gmail.com>
2021-11-30 15:25:37 +0100

    Add a placeholder

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Placeholder from './components/Placeholder';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Placeholder />
       <Footer />
     </div>
   );
---
+++ src/components/Placeholder.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import placeholder from '../assets/images/placeholder.png';
+
+const Main = () => (
+  <img src={placeholder} alt="Placeholder" />
+
+);
+export default Main;
