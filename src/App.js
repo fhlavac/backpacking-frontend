9c7c53d563f44079f89a7dcb75ca5e8ccfbd3a3b
Radek Kalužík <radek.kaluzik@email.cz>
2021-11-30 11:25:53 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.scss';
+import './style.scss';
+import { Button } from '@patternfly/react-core';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="primary">Primary</Button>{' '}
+      </header>
+    </div>
+  );
+}
+
+export default App;
