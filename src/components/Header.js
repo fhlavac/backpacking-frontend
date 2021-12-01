5fc0375f48683299ea88c6253c05bf2ad62aa4ed
Radek Kalužík <radek.kaluzik@email.cz>
2021-12-01 10:41:42 +0100

    content, category list

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import {
-  Masthead, MastheadToggle, MastheadMain, MastheadBrand, MastheadContent, Button,
+  Masthead, MastheadMain, MastheadBrand, MastheadContent,
 } from '@patternfly/react-core';
-import BarsIcon from '@patternfly/react-icons/dist/js/icons/bars-icon';
 import logo from '../assets/images/logo.svg';
 
 const Header = () => (
   <Masthead id="basic">
-    <MastheadToggle>
-      <Button variant="plain" onClick={() => {}} aria-label="Global navigation">
-        <BarsIcon />
-      </Button>
-    </MastheadToggle>
     <MastheadMain>
       <MastheadBrand>
         <img src={logo} alt="Patterfly Logo" />
       </MastheadBrand>
     </MastheadMain>
     <MastheadContent>
-      <span>Content</span>
+      <span>My Gear</span>
     </MastheadContent>
   </Masthead>
 );
