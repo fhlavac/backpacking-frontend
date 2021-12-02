112b5562cd3c9f5ded9560f6842a0eef57caeedf
ewinchel <ewinchel@redhat.com>
2021-12-02 13:36:32 -0500

    update 'add gear' screen

--- src/components/AddGear.js
+++ src/components/AddGear.js
@@ -1,18 +1,43 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
+  FileUpload,
+  Grid,
   Page,
   PageSection,
+  PageSectionVariants,
   Title,
+  Form, FormGroup, TextInput,
 } from '@patternfly/react-core';
 
 const AddGear = () => (
   <Page>
-    <PageSection className="app-c-page-section">
+    <PageSection variant={PageSectionVariants.light} className="app-c-page-section">
       <Title headingLevel="h2" size="xl" className="app-c-title pf-u-pb-md">
         Add Gear
       </Title>
       <Title headingLevel="h3" size="l" className="app-c-title pf-u-pb-md" />
+      <Form>
+        <Grid span={7} asGutter>
+          <FormGroup label="Name">
+            <TextInput />
+          </FormGroup>
+          <FormGroup label="Description">
+            <TextInput />
+          </FormGroup>
+          <FormGroup label="Weight (grams)">
+            <TextInput />
+          </FormGroup>
+          <br />
+          <FormGroup label="Upload gear photo">
+            <FileUpload
+              type="text"
+              filenamePlaceholder="Drag and drop a file or upload one"
+              browseButtonText="Upload"
+            />
+          </FormGroup>
+        </Grid>
+      </Form>
     </PageSection>
   </Page>
 );
